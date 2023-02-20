5b87babc3fa655d4849a39b6c1ede9c0910581ca
bahodirovu <bahodirovumid1@gmail.com>
2023-02-20 11:14:07 +0300

    add css

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,21 @@
 import React from "react";
+import "./App.css";
 
 const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <ol>
-        {ingredients.map((ingredient) => (
-          <li>{ingredient.text}</li>
-        ))}
-      </ol>
-      <p>{calories}</p>
-      <img src={image} alt="" />
+    <div className="recipe">
+      <div className="text">
+        <h1>{title}</h1>
+        <ol>
+          {ingredients.map((ingredient) => (
+            <li>{ingredient.text}</li>
+          ))}
+        </ol>
+        <p>{calories}</p>
+      </div>
+      <div className="img">
+        <img src={image} alt="" />
+      </div>
     </div>
   );
 };
